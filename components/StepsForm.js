b4c2b56eab5066793ea83cdc1943b2612c610739
TorunW <torun.wikstrom@gmail.com>
2023-09-22 15:59:34 +0200

    styling instructions form

--- components/StepsForm.js
+++ components/StepsForm.js
@@ -1,15 +1,22 @@
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Pressable } from 'react-native';
 import React, { useState } from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
-import { COLORS, FONTS, SIZES } from '../constants';
+import { COLORS, FONTS, SIZES, SPACING } from '../constants';
 import { useStoreActions } from 'easy-peasy';
+import { RemoveIcon } from '../assets/icons/Icon';
+import { ActivityIndicator } from 'react-native';
 
-const StepsForm = (setIsInstructionsSubmitted) => {
+const StepsForm = ({ setIsInstructionsSubmitted }) => {
   const setInstructionsToDrink = useStoreActions(
     (actions) => actions.drinks.setInstructionsToDrink
   );
+  const [isLoading, setIsLoading] = useState(false);
 
-  const { register, control, handleSubmit, reset } = useForm();
+  const { register, control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      instructions: [{ step: '' }],
+    },
+  });
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'instructions',
@@ -32,41 +39,17 @@ const StepsForm = (setIsInstructionsSubmitted) => {
   };
 
   return (
-    <View
-      style={{
-        paddingHorizontal: 20,
-        backgroundColor: COLORS.secondary,
-        paddingVertical: 10,
-        gap: 5,
-        justifyContent: 'center',
-      }}
-    >
-      <Text style={{ fontFamily: FONTS.bold, fontSize: SIZES.large }}>
-        Add instructions
-      </Text>
+    <View style={styles.pageContainer}>
+      <Text style={styles.title}>Add instructions</Text>
       <View>
         {fields.map((item, index) => {
           return (
-            <View
-              key={item.id}
-              style={{
-                width: '100%',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}
-            >
+            <View key={item.id} style={styles.formContainer}>
               <Controller
                 render={({ field: { onChange, onBlur, value } }) => (
                   <TextInput
-                    style={{
-                      elevation: 1,
-                      width: '70%',
-                      borderRadius: 3,
-                      paddingLeft: 4,
-                      backgroundColor: COLORS.grad3,
-                    }}
-                    placeholder='Instructions'
+                    style={styles.textInput}
+                    placeholder={`Step ${index + 1}`}
                     onBlur={onBlur}
                     onChangeText={onChange}
                     value={value}
@@ -78,42 +61,94 @@ const StepsForm = (setIsInstructionsSubmitted) => {
                 name={`instructions.${index}.step`}
                 control={control}
               />
-              <Button
-                style={{
-                  width: '10%',
-                }}
-                color='red'
-                title='-'
-                onPress={() => remove(index)}
-              />
+              <Pressable onPress={() => remove(index)}>
+                <RemoveIcon color={COLORS.deepPink} size={SIZES.iconL} />
+              </Pressable>
             </View>
           );
         })}
       </View>
-      <View style={{ gap: 5 }}>
-        <Button
-          title='+ Add another step'
-          onPress={() => {
-            append({ step: '' });
-          }}
-        />
-
-        <Button
-          title='Reset'
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          append({ ingredient: '', measure: '' });
+        }}
+      >
+        <Text style={styles.font}>+ Add Another Step to Instructions</Text>
+      </Pressable>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={styles.button}
           onPress={() =>
             reset({
-              test: [{ step: '' }],
+              test: [{ ingredient: '', measure: '' }],
             })
           }
-        />
+        >
+          <Text style={styles.font}>Reset</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={handleSubmit(onSubmit)}>
+          {isLoading === false ? (
+            <Text style={styles.font}>Submit</Text>
+          ) : (
+            <ActivityIndicator size='small' color={COLORS.black} />
+          )}
+        </Pressable>
       </View>
-
-      <Button
-        title={isLoading === true ? 'is submitting' : 'submit'}
-        onPress={handleSubmit(onSubmit)}
-      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    paddingHorizontal: SPACING.m,
+    paddingVertical: SPACING.l,
+    backgroundColor: COLORS.midPink,
+    gap: SPACING.xs,
+  },
+  title: {
+    fontFamily: FONTS.bold,
+    fontSize: SIZES.large,
+    marginBottom: SPACING.s,
+    textAlign: 'center',
+  },
+  formContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: SPACING.s,
+  },
+  textInput: {
+    width: '80%',
+    borderRadius: 5,
+    paddingVertical: SPACING.xs,
+    paddingHorizontal: SPACING.xs,
+    backgroundColor: COLORS.pinkTransparent,
+    borderWidth: 0.9,
+    borderColor: COLORS.deepPinkTransparent,
+    fontFamily: FONTS.regular,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    gap: SPACING.s,
+    marginTop: SPACING.xs,
+  },
+  button: {
+    minWidth: '50%',
+    borderRadius: 5,
+    paddingVertical: SPACING.xs,
+    paddingHorizontal: SPACING.xs,
+    backgroundColor: COLORS.deepPink,
+    borderWidth: 0.9,
+    borderColor: COLORS.deepPinkTransparent,
+    marginVertical: SPACING.xs,
+  },
+  font: {
+    textAlign: 'center',
+    fontFamily: FONTS.medium,
+  },
+});
 
 export default StepsForm;
